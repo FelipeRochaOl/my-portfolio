56b07e755018e6eb73f19ed6b8b03252d87ab801
Felipe Rocha <feliperochaoliveira@gmail.com>
2021-01-05 02:59:45 -0300

    App finalized

--- src/app.js
+++ src/app.js
@@ -1,33 +1,87 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
+const repositories = []
 
-app.get("/repositories", (request, response) => {
-  // TODO
-});
+const validateRepositoryId = (req, res, next) => {
+  const { id } = req.params
+  const repository = repositories.findIndex(repository => repository.id === id)
 
-app.post("/repositories", (request, response) => {
-  // TODO
-});
+  if (repository === -1) {
+    return res.status(400).json({})
+  }
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+  req.params.index = repository
+  next()
+}
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+function existsKeys(obj) {
+  const keys = Object.keys(obj)
+  return keys.includes('title', 'url', 'techs')
+}
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+function isUndefined(obj) {
+  const undefined = Object.values(obj)
+                          .filter(value => value === null || value === '')
+  return undefined.length > 0
+}
 
-module.exports = app;
+const validateData = (req, res, next) => {
+  if (!existsKeys(req.body) || isUndefined(req.body)) {
+    return res.status(404).json({ message: 'Inputs emptys' })
+  }
+
+  if (!Array.isArray(req.body.techs)) {
+    return res.status(400).json({ message: 'Input techs is not array' })
+  }
+
+  return next()
+}
+
+app.get('/repositories', (req, res) => {
+  return res.status(200).json(repositories)
+})
+
+app.post('/repositories', /*validateData*/ (req, res) => {
+  const { title, url, techs } = req.body
+  repositories.push({id: uuid(), title, url, techs, likes: 0})
+
+  return res.status(201).json(repositories)
+})
+
+app.put('/repositories/:id', validateRepositoryId, /*validateData*/ (req, res) => {
+  const { index } = req.params
+  const { title, url, techs } = req.body
+  //poderia fazer {...repositories[index]}, mas manter o padrão do obj
+  repositories[index] = { 
+    id: repositories[index].id , 
+    title, 
+    url, 
+    techs, 
+    likes: repositories[index].likes
+  }
+
+  return res.status(200).json(repositories)
+})
+
+app.delete('/repositories/:id', validateRepositoryId, (req, res) => {
+  const { index } = req.params
+  repositories.splice(index, 1)
+  return res.status(204).json(repositories)
+})
+
+app.post('/repositories/:id/like', validateRepositoryId, (req, res) => {
+  const { index } = req.params
+  repositories[index].likes += 1
+
+  return res.status(201).json({ likes: repositories[index].likes })
+})
+
+module.exports = app
